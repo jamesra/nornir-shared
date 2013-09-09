338b041b4f959fd29c0c97386a31578b834861e0
James Anderson <james.r.andreson@utah.edu>
2013-09-09 14:56:49 -0700

    Fix for recursive test entry by testing for '__main__'

--- setup.py
+++ setup.py
@@ -10,13 +10,14 @@ from ez_setup import use_setuptools
 from setuptools import setup, find_packages
 import sys
 
-use_setuptools()
+if __name__ == '__main__':
+    use_setuptools()
 
-setup(name='nornir_shared',
-      version='1.0',
-      description="Shared routines for Nornir python packages and scripts",
-      author="James Anderson",
-      author_email="James.R.Anderson@utah.edu",
-      url="https://github.com/jamesra/nornir-shared",
-      packages=["nornir_shared"],
-      test_suite='test')
+    setup(name='nornir_shared',
+          version='1.0',
+          description="Shared routines for Nornir python packages and scripts",
+          author="James Anderson",
+          author_email="James.R.Anderson@utah.edu",
+          url="https://github.com/jamesra/nornir-shared",
+          packages=["nornir_shared"],
+          test_suite='test')
