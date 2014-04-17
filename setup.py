5ca03321c851312647e31bd7cc4eb272f3e044c4
James Anderson <james.r.andreson@utah.edu>
2014-04-17 14:52:28 -0700

    Updated setup for python 3

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ Created on Aug 30, 2013
 @author: u0490822
 '''
 
-
-
 from ez_setup import use_setuptools
-from setuptools import setup, find_packages
-import sys
+
 
 if __name__ == '__main__':
     use_setuptools()
+
+    from setuptools import setup
+
+    install_requires = ["six"]
 
     setup(name='nornir_shared',
           version='1.1.7',
@@ -20,4 +21,5 @@ if __name__ == '__main__':
           author_email="James.R.Anderson@utah.edu",
           url="https://github.com/jamesra/nornir-shared",
           packages=["nornir_shared"],
+          install_requires=install_requires,
           test_suite='test')
