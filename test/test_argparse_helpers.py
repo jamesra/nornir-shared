bb81e008275798666f03f9913a026d699dab22cf
James Anderson <james.r.anderson@utah.edu>
2023-02-17 15:20:40 -0800

    Added tests for integer and float ranges

--- test/test_argparse_helpers.py
+++ test/test_argparse_helpers.py
@@ -14,9 +14,14 @@ class ArgParseHelpersTest(unittest.TestCase):
 
     def testFloatRange(self):
         
+        self.assertTrue(nornir_shared.argparse_helpers.FloatRange('-5:5:5') == [-5, 0, 5])
+        
         self.assertTrue(nornir_shared.argparse_helpers.FloatRange('0') == [0.0])
         self.assertTrue(nornir_shared.argparse_helpers.FloatRange('0:3') == [0.0, 1.0, 2.0, 3.0])
         self.assertTrue(nornir_shared.argparse_helpers.FloatRange('0:0.5:2') == [0.0, 0.5, 1.0, 1.5, 2.0])
+        
+        
+        
         
         pass
     
@@ -42,7 +47,7 @@ class ArgParseHelpersTest(unittest.TestCase):
         self.assertTrue(nornir_shared.argparse_helpers.IntegerList('1,3-5') == [1, 3, 4, 5])
         self.assertTrue(nornir_shared.argparse_helpers.IntegerList('3-5,7') == [3, 4, 5, 7])
         self.assertTrue(nornir_shared.argparse_helpers.IntegerList('1,3-5,7') == [1, 3, 4, 5, 7])
-        self.assertTrue(nornir_shared.argparse_helpers.IntegerList('1,7') == [1, 7])
+        self.assertTrue(nornir_shared.argparse_helpers.IntegerList('1,7,9,10-12,15') == [1, 7, 9, 10, 11, 12, 15])
         
         pass
 
