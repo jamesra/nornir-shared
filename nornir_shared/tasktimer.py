7c20ad67f41d7ae99339e3d584cc4e275be4ec7b
James Anderson <james.r.anderson@utah.edu>
2025-04-01 19:32:34 -0700

    Added a context aware version of TaskTimer

--- nornir_shared/tasktimer.py
+++ nornir_shared/tasktimer.py
@@ -1,12 +1,13 @@
 import datetime
 import math
 import time
+import inspect
 
 from . import prettyoutput
 
 
 class TaskTimer:
-    '''Helper class that allows us to manually record how long a given section of code took to execute'''
+    """Helper class that allows us to manually record how long a given section of code took to execute"""
 
     def __init__(self):
         # Dictionary of start times
@@ -24,7 +25,7 @@ class TaskTimer:
         self._TaskStartTime[task] = time.perf_counter()
 
     def End(self, task, print_elapsed=True):
-        '''Stop a timer for a task, print output if PrintElapsed is not explicitely set to False'''
+        """Stop a timer for a task, print output if PrintElapsed is not explicitely set to False"""
         tend = time.perf_counter()
 
         tstart = self.DefaultStartTime
@@ -48,7 +49,7 @@ class TaskTimer:
             [ms, sec] = math.modf(tdelta)
             [garbage, ms] = math.modf(ms * 1000)
             return task + ' : %2d days %s' % (
-            tdelta / (60 * 60 * 24), time.strftime('%H:%M:%S', time.gmtime(tdelta))) + '.' + ('%d' % ms)
+                tdelta / (60 * 60 * 24), time.strftime('%H:%M:%S', time.gmtime(tdelta))) + '.' + ('%d' % ms)
 
         return ''
 
@@ -62,3 +63,25 @@ class TaskTimer:
             s += f'\n\t{outstr}'
 
         return s
+
+
+class TaskTimerContext(TaskTimer):
+    """A context manager for the TaskTimer class.  Can be used to time a block of code.  When the context is ended all task timers are stopped"""
+
+    _current_task = None
+
+    @staticmethod
+    def _get_calling_function_name():
+        frame = inspect.currentframe()
+        caller_frame = frame.f_back.f_back
+        return caller_frame.f_code.co_name
+
+    def __enter__(self):
+        self._current_task = self._get_calling_function_name()
+        self.Start(self._current_task)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        task_names = list(self._TaskStartTime.keys())
+        for task_name in task_names:
+            self.End(task_name, print_elapsed=False)
